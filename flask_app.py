dfdaa48cc938c5e1e09fb8bcc67b06e13c576cb8
Lauren Newbould <lauren.newbould@bbc.co.uk>
2019-10-02 15:47:58 +0100

    added ability to get and post

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 # flask_app.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import pandas as pd 
 
 app = Flask(__name__)
@@ -10,9 +10,18 @@ app.config["DEBUG"] = True
 def hello():
     return "Hello Worlds!"
 
-@app.route("/api")
+@app.route("/api", methods=["POST", "GET"])
 def j():
-	data = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
-	json_data = data.to_json(orient='records')
-	return json_data
+	if request.methods == "POST": 
+		post_data = request.form.to_dict(flat=False)
+		print(post_data)
+		post_data_df = pd.DataFrame(post_data)
+		print(post_data_df)
+		post_data_json = post_data_df.to_json(orient='records')
+		print(post_data_json)
+		return post_data_json
+
+	else if request.methods == "GET":
+		post_data = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]}).to_json(orient='records')
+		return post_data
 
